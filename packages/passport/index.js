387ed4925980c297b131505b3aad8889ff4887f3
moubuns <mengwl@fox.one>
2019-01-21 21:09:45 +0800

    fix wrong spell

--- packages/passport/index.js
+++ packages/passport/index.js
@@ -1,6 +1,6 @@
 
 const Passport = require('./src/passport');
-const Token = require('./src/tokn');
+const Token = require('./src/token');
 const Sign = require('./src/sign');
 
 module.exports = Passport.default;
