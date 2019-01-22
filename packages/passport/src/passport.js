cfa0beecf2a0006b1450edaedaac2975641e7d26
moubuns <mengwl@fox.one>
2019-01-22 11:15:44 +0800

    update variable name

--- packages/passport/src/passport.js
+++ packages/passport/src/passport.js
@@ -5,29 +5,29 @@ import { generateToken } from './token';
 export default class Passport {
   constructor(props) {
     super.constructor(props)
-    this.API_Host = props.API_Host;
-    this.merchant_id = props.merchant_id
+    this.ApiHost = props.ApiHost;
+    this.merchantId = props.merchantId
   }
 
   async getCaptcha() {
     const url = '/api/captcha';
     const signData = generateSignRequest('post', url)
-    let uri = `${this.API_Host}${signData.uri}`
+    let uri = `${this.ApiHost}${signData.uri}`
     let res = await http.post(uri)
     let data = {
       captchaId: res.captcha_id,
-      captchaURL: `${this.API_Host}/api/captcha/${res.captcha_id}.png`
+      captchaURL: `${this.ApiHost}/api/captcha/${res.captcha_id}.png`
     }
     return data
   }
 
-  async requestRegisterSMS(regionCode, mobile, captureId, captureCode) {
+  async requestRegisterSMS(regionCode, mobile, captchaId, captchaCode) {
     const url = '/api/account/request_register_phone';
     const data = {
       phone_code: regionCode,
       phone_number: mobile,
-      captcha_id: captureId,
-      capture: captureCode
+      captcha_id: captchaId,
+      capture: captchaCode
     }
 
     return await this.postRequest(generateSignRequest('post', url, data));
@@ -44,14 +44,14 @@ export default class Passport {
     return await this.postRequest(generateSignRequest('post', url, data));
   }
 
-  async requestLoginSMS(regionCode, mobile, captureId, captureCode) {
+  async requestLoginSMS(regionCode, mobile, captchaId, captchaCode) {
     const url = '/api/account/request_login_phone';
     const data = {
       phone_code: regionCode,
       phone_number: mobile,
-      captcha_id: captureId,
-      capture: captureCode
-    } 
+      captcha_id: captchaId,
+      capture: captchaCode
+    }
 
     generateSignRequest('post', url, data)
 
@@ -83,20 +83,20 @@ export default class Passport {
     const url = '/api/account/detail';
     const signData = generateSignRequest('get', url);
     const token = await generateToken(key, secret, signData.sign)
-    const uri = `${this.API_Host}${signData.uri}`
+    const uri = `${this.ApiHost}${signData.uri}`
 
-    return await http.get(uri, { headers: { "Authorization": `Bearer ${token}`, ...this.defaulutHeader() }})
+    return await http.get(uri, { headers: { "Authorization": `Bearer ${token}`, ...this.defaulutHeader() } })
   }
 
   async postRequest(signData) {
-    const uri = `${this.API_Host}${signData.uri}`
+    const uri = `${this.ApiHost}${signData.uri}`
     const headers = { headers: this.defaulutHeader() }
     return await http.post(uri, signData.newBody, headers)
   }
 
   defaulutHeader() {
     return {
-      "fox-cloud-merchant-id": this.merchant_id
+      "fox-cloud-merchant-id": this.merchantId
     }
   }
 }
