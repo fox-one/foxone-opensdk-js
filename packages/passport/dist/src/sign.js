60ec53cf99035629ebea5e925db80c74c15bead7
moubuns <mengwl@fox.one>
2019-01-23 12:19:52 +0800

    update sign to ts

--- packages/passport/dist/src/sign.js
+++ packages/passport/dist/src/sign.js
@@ -1,19 +1,55 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var uuid_1 = require("uuid");
 var CryptoJS = require("crypto-js");
-function generateSignRequest(_a) {
-    var method = _a.method, url = _a.url, _b = _a.body, body = _b === void 0 ? null : _b;
+var token_1 = require("./token");
+function generateSignRequest(request) {
     var ts = getTimestamp();
     var nonce = uuid_1.v4();
-    var uri = url;
+    var uri = request.url;
+    var body = request.body;
     if (uri.indexOf('?') > 0) {
         uri = uri + "_ts=" + ts + "&_nonce=" + nonce;
     }
     else {
         uri = uri + "?_ts=" + ts + "&_nonce=" + nonce;
     }
-    var message = "" + method.toUpperCase() + uri;
+    var message = "" + request.method.toUpperCase() + uri;
     if (body) {
         message += JSON.stringify(body);
     }
@@ -22,6 +58,33 @@ function generateSignRequest(_a) {
     return { uri: uri, body: body, sign: sign };
 }
 exports.generateSignRequest = generateSignRequest;
+function generateSignAndJWT(request) {
+    return __awaiter(this, void 0, void 0, function () {
+        var method, url, body, key, secret, signData, sign, keyAndSign, token;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    method = request.method;
+                    url = request.url;
+                    body = request.body;
+                    key = request.key;
+                    secret = request.secret;
+                    signData = generateSignRequest({ method: method, url: url, body: body });
+                    sign = signData.sign;
+                    keyAndSign = {
+                        key: key,
+                        secret: secret,
+                        requestSign: sign
+                    };
+                    return [4 /*yield*/, token_1.generateToken(keyAndSign)];
+                case 1:
+                    token = _a.sent();
+                    return [2 /*return*/, { url: url, body: body, headers: { "Authorization": "Bearer " + token } }];
+            }
+        });
+    });
+}
+exports.generateSignAndJWT = generateSignAndJWT;
 function getTimestamp(date) {
     if (date === void 0) { date = new Date(); }
     return Math.round(date.getTime() / 1000);
---
+++ packages/passport/src/sign.ts
@@ -0,0 +1,49 @@
+import { v4 as uuid } from 'uuid';
+import * as CryptoJS from 'crypto-js';
+import { generateToken } from './token';
+
+export function generateSignRequest(request: { method: string, url: string, body?: any }) {
+  let ts = getTimestamp();
+  let nonce = uuid();
+  let uri = request.url;
+  let body = request.body;
+
+  if (uri.indexOf('?') > 0) {
+    uri = `${uri}_ts=${ts}&_nonce=${nonce}`
+  } else {
+    uri = `${uri}?_ts=${ts}&_nonce=${nonce}`
+  }
+
+  let message = `${request.method.toUpperCase()}${uri}`
+  if (body) {
+    message += JSON.stringify(body)
+  }
+
+  let digiest = CryptoJS.SHA256(message);
+  let sign = CryptoJS.enc.Base64.stringify(digiest);
+  return { uri, body, sign }
+}
+
+export async function generateSignAndJWT(request: { method: string, url: string, body?: any, key: string, secret: string }) {
+  let method = request.method;
+  let url = request.url;
+  let body = request.body;
+  let key = request.key;
+  let secret = request.secret;
+
+  const signData = generateSignRequest({ method, url, body });
+  const sign = signData.sign;
+
+  const keyAndSign = {
+    key,
+    secret,
+    requestSign: sign
+  }
+
+  let token = await generateToken(keyAndSign);
+  return { url, body, headers: { "Authorization": `Bearer ${token}` } }
+}
+
+function getTimestamp(date = new Date()) {
+  return Math.round(date.getTime() / 1000)
+}
